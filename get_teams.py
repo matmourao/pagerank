8ef8053bb225dc9fa8e6c3191e1388d72aa6b6a7
Luana Hatab <luanahatab@gmail.com>
2022-10-18 14:52:21 -0300

    cria df

---
+++ get_teams.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+req = requests.get("https://esportes.estadao.com.br/classificacao/futebol/campeonato-brasileiro-serie-a/2021")
+soup = BeautifulSoup(req.content, "html.parser")
+
+df = open('dataframe.csv', 'w', encoding='utf-8-sig')
+writer = csv.writer(df)
+header = ["rodada", "time1", "time2", "gol1", "gol2"]
+writer.writerow(header)
+
+for rodada in soup.find_all(class_="swiper-slide"):
+    for jogo in rodada.find_all(class_="item"):
+        row = [rodada.find(class_="table-fase").text]
+        for time in jogo.find(class_="details").find_all(class_="shortname"):
+            row.append(time.text)
+        for gols in jogo.find(class_="details").find_all(class_="goal"):
+            row.append(gols.text)
+        writer.writerow(row)
+   
+df.close()
