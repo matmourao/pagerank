a395e3c3acfd6c6820c0c4ed3057d3027d084abd
Mateus Mourão <99206764+matmourao@users.noreply.github.com>
2022-10-18 18:58:53 -0300

    df arestas

--- get_data.py
+++ get_data.py
@@ -9,6 +9,7 @@ data = open('data.csv', 'w', encoding='utf-8-sig')
 writer = csv.writer(data)
 header = ["rodada", "time1", "time2", "gol1", "gol2"]
 writer.writerow(header)
+partidas = []
 
 for rodada in soup.find_all(class_="swiper-slide"):
     for jogo in rodada.find_all(class_="item"):
@@ -18,5 +19,37 @@ for rodada in soup.find_all(class_="swiper-slide"):
         for gols in jogo.find(class_="details").find_all(class_="goal"):
             row.append(gols.text)
         writer.writerow(row)
-   
+        partidas.append(row)
 data.close()
+
+perdedores = []
+vencedores = []
+for partida in partidas:
+    if partida[3] < partida[4]:
+        perdedores.append(partida[1])
+        vencedores.append(partida[2])
+    if partida[4] < partida[3]:
+        perdedores.append(partida[2])
+        vencedores.append(partida[1])
+
+arestas = []
+for i in range(len(perdedores)):
+  arestas.append([perdedores[i], vencedores[i]])
+
+pesos = [0]*len(arestas)
+for i in range(len(arestas)):
+  if arestas[i] == []:
+    continue
+  for j in range(len(arestas)):
+    if arestas[i] == arestas[j]:
+      pesos[i] += 1
+      if i != j:
+        arestas[j] = []
+for a in arestas:
+  a.append(pesos[(arestas.index(a))])
+arestas = list(filter(lambda x: x != [0], arestas))
+
+with open("arestas.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["source","target","weight"])
+    writer.writerows(arestas)
